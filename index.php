748b3f2292ae92cc134bd885d7fb083d84e3fa09
Adrian Devto <hgperu@gmail.com>
2021-12-14 16:42:45 -0500

    Agregue un index al tutorial

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title></title>
+</head>
+<body>
+<h1>TUtorial de PHP</h1>
+<div>
+  <ul>
+    <li><a href="variables.php">Variables</a></li>
+    <li><a href="operaciones.php">Operaciones</a></li>
+    <li><a href="condicionales.php">Condicionales</a></li>
+    <li><a href=""></a></li>
+  </ul>
+</div>
+</body>
+</html>
